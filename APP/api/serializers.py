8be737f9f749961d05c169528a6a2a5017672c93
deveshwarule-tudip <devesh.warule@tudip.com>
2022-12-12 12:31:38 +0530

    Nested Serializer

--- APP/api/serializers.py
+++ APP/api/serializers.py
@@ -1,19 +1,21 @@
 from rest_framework import serializers
 from APP.models import WatchList,StreamPlatform
 
-
-    
-class StreamPlatformSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = StreamPlatform
-        fields = "__all__"
-
 class WatchListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = WatchList
         fields = "__all__"
+    
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    WatchList = WatchListSerializer(many=True, read_only=True)
+
+    
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
+
+
         
         #fields = ['id', 'name', 'description']
         #exculde = ['active']
