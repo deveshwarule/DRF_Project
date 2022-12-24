37d5f3294509f780908dfe6bebf8b4d50bf881d5
deveshwarule-tudip <devesh.warule@tudip.com>
2022-12-24 18:11:10 +0530

    Basic Auth added

--- APP/api/urls.py
+++ APP/api/urls.py
@@ -9,7 +9,7 @@ router.register('stream',StreamPlatformVS,basename='streamplatform')
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(),name='movies_list'),
-    path('<int:pk>', WatchDetailAV.as_view(),name='movies_detail'),
+    path('<int:pk>/', WatchDetailAV.as_view(),name='movies_detail'),
     path('',include(router.urls)),
     
     # path('stream/', StreamPlatformAV.as_view(),name='stream'),
@@ -18,8 +18,8 @@ urlpatterns = [
     # path('review/', ReviewList.as_view(),name='review-list'),
     # path('review/<int:pk>', ReviewDetail.as_view(),name='stream-detail'),
     
-    path('<int:pk>/review-create', ReviewCreate.as_view(),name='Review-create'),
-    path('<int:pk>/reviews', ReviewList.as_view(),name='Review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(),name='review-detail'),
+    path('<int:pk>/review-create/', ReviewCreate.as_view(),name='Review-create'),
+    path('<int:pk>/reviews/', ReviewList.as_view(),name='Review-list'),
+    path('review/<int:pk>/', ReviewDetail.as_view(),name='review-detail'),
     
 ]
